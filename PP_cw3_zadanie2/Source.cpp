2df4df9609d9eeff645ccf05bc24c7e6795812e2
Miroslaw Puczynski <miroslaw@puczynski.org>
2019-11-12 07:30:56 +0100

    001

--- PP_cw3_zadanie2/Source.cpp
+++ PP_cw3_zadanie2/Source.cpp
@@ -14,18 +14,25 @@ void  wyznacz_pierwiastki_rown_kw() {
 	cout << "Wspolczynik b = "; cin >> b;
 	cout << "Wyraz wolny c = "; cin >> c;
 	delta = b * b - 4 * a * c;
-	float skladnik1 = -b / (2 * c);
+	float skladnik1 = -b / (2 * a);
 	float skladnik2 = sqrt(abs(delta)) / (2 * a);
 
 	cout << "****************** wyniki obliczen ******************" << endl;
 	if (delta > 0) {
-		cout << "x1" << skladnik1 - skladnik2 << endl;
-		cout << "x2" << skladnik1 + skladnik2;
+		cout << "x1 = " << skladnik1 - skladnik2 << endl;
+		cout << "x2 = " << skladnik1 + skladnik2;
+	}
+	else if (delta < 0){
+		cout << "z1 = " << skladnik1 << '-' << skladnik2 << "i" << endl;
+		cout << "z2 = " << skladnik1 << '+' << skladnik2 << "i";
 	}
 	else {
-
+		cout << "x0 = " << skladnik1;
 	}
+}
 
+int main() {
+	wyznacz_pierwiastki_rown_kw();
 
 
 }
